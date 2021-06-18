18667935f589cf89697833a828d480310bd6fe8f
Vasta1921 <55996682+Vasta1921@users.noreply.github.com>
2021-06-18 16:19:07 +0300

    1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -14,30 +14,6 @@ public class Util extends Config {
         return connection;
     }
 
-   /* private static Connection getMysqlConnection() {
-        try {
-            DriverManager.registerDriver((Driver) Class
-                    .forName("com.mysql.jdbc.Driver").newInstance());
 
-            StringBuilder url = new StringBuilder();
-
-            url.
-                    append("jdbc:mysql://").        //db type
-                    append("localhost:").           //host name
-                    append("3306/").                //port
-                    append("myshema").          //db name
-                    append("user=root&").          //login
-                    append("password=root").       //password
-                    append("&serverTimezone=UTC");
-
-            System.out.println("URL: " + url + "\n");
-
-            Connection connection = DriverManager.getConnection(url.toString());
-            return connection;
-        } catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            e.printStackTrace();
-            throw new IllegalStateException();
-        }
-    }*/
 
 }
